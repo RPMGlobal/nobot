003c081214792a51c2e0d66d15759dc36008568d
joelwang0528 <shuyingrr@hotmail.com>
2017-06-02 11:26:15 +1000

    Add Congnitive nuget package

---
+++ nobot/nobot/Dialogs/QnaStackOverflowDialog.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nobot.Dialogs
+{
+    [Serializable]
+    [QnAMaker("set yout subscription key here", "set your kbid here", "I don't understand this right now! Try another query!", 0.50, 3)]
+    public class QnaStackOverflowDialog : QnAMakerDialog
+    {
+    }
+}
